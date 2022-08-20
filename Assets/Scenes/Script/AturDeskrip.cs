0c504f2c201f932034ac5f781d8668afecea5de9
TehCelup <jonatalsitanggang89@gmail.com>
2022-08-21 00:27:26 +0700

    Files

---
+++ Assets/Scenes/Script/AturDeskrip.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AturDeskrip : MonoBehaviour
+{
+    // [Header("Deskripsi")]
+    // public TrackableAR[] tr;
+    // public string[] nama;
+    // [TextArea]
+    // public string[] deskripsi;
+
+    // [Header("UI Deskripsi")]
+    // public Text txtNama;
+    // public Text txtDeskripsi;
+    // public GameObject goNama;
+    // public GameObject goDeskripsi;
+
+    // public bool[] cekMarker;
+    // int countMarker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // cekMarker = new bool[tr.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //     for (int i = 0; i < tr.length; i++)
+    //     {
+    //         if (tr[i].GetMarker())
+    //         {
+    //             txtNama.text = nama[i];
+    //             txtDeskripsi.text = deskripsi[i];
+
+    //             if (!cekMarker[i])
+    //             {
+    //                 countMarker++;
+    //                 cekMarker[i] = true;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             if (cekMarker[i])
+    //             {
+    //                 countMarker--;
+    //                 cekMarker[i] = false;
+    //             }
+    //         }
+    //     }
+    //     DeskripsiPanel();
+    // }
+    // private void DeskripsiPanel()
+    // {
+    //     if (countMarker == 0)
+    //     {
+    //         goNama.SetActive(false);
+    //         goDeskripsi.SetActive(false);
+    //     }
+    //     else
+    //     {
+    //         goNama.SetActive(true);
+    //         goDeskripsi.SetActive(true);
+    //     }
+    // }
+}
